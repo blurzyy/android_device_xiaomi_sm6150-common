e0b22f6124c79b86fcfd240a09c85dec303ee5bb
yashlearnpython <73551736+yash3056@users.noreply.github.com>
2021-07-25 20:22:24 +0530

    sm6150-common: Add permission for Xiaomiparts
    this is being added to stop xiaomiparts being killed
    
    sm8150-common: Parts: move permission to system_ext
    
    07-28 20:06:35.938  1454  1454 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: main
    07-28 20:06:35.938  1454  1454 E AndroidRuntime: java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions whitelist: {org.lineageos.settings (/system_ext/priv-app/XiaomiParts): android.permission.WRITE_SECURE_SETTINGS, org.lineageos.settings (/system_ext/priv-app/XiaomiParts): android.permission.INTERACT_ACROSS_USERS}
    
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -169,6 +169,9 @@ PRODUCT_COPY_FILES += \
 # Device Specific Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
+	
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
 
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
