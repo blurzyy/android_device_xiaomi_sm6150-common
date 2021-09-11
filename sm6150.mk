476b47e5938208ff3543648ec138392e150f5b55
Peter Cai <peter@typeblog.net>
2021-09-11 19:06:47 +0530

    sm6150-common: Add libstagefright-omx & android.hardware.media.omx@1.0-service
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -290,6 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+		android.hardware.media.omx@1.0-service \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libarbitrarybytes \
@@ -306,7 +307,8 @@ PRODUCT_PACKAGES += \
     libOmxSwVencMpeg4 \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+		libstagefright_omx.vendor
 
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
